eca6a1a1bf902a2268ec855a1601f949583458fc
Yury <iurii.malovichko@gmail.com>
2025-11-21 14:58:12 +0100

    Support for projections postfixes in the Rust code

--- src/rust/src/bin/filter_fasta.rs
+++ src/rust/src/bin/filter_fasta.rs
@@ -100,7 +100,10 @@ fn main() {
                     // do not record the sequence line if it does not belong to query
                     if !is_query {record = false; continue}
                 }
-                let proj = comps[0].replace('>', "");
+                let proj = comps[0]
+                    .replace('>', "")
+                    .replace("#retro", "")
+                    .replace("#paralog", "");
                 // do not record the deprecated projection's sequence
                 // if excluded_projs.contains(&proj) {record = false; continue}
                 if included_projs.len() > 0 && !included_projs.contains(&proj) {continue}
